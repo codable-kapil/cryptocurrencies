827fd28b9e9a60a0119c8d8695a4fcc951d69494
codable-kapil <kapil.shivhare09@gmail.com>
2024-09-27 11:30:27 -0700

    Initial Commit

---
+++ CryptoCurrencies/CryptoCurrenciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrenciesApp.swift
+//  CryptoCurrencies
+//
+//  Created by Kapil Shivhare on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrenciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
